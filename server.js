b0c5a8164b762fcbbf652e0eb25a6fd57296c801
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-08-03 19:36:13 +0100

    Add API endpoints

--- server.js
+++ server.js
@@ -8,3 +8,38 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get("/api/quotes/random", (req, res) => {
+   res.send({
+      quote: getRandomElement(quotes)
+   });
+});
+
+app.get("/api/quotes", (req, res) => {
+   if (req.query.person !== undefined) {
+      const quotesByPerson = quotes.filter(quote => quote.person === req.query.person);
+      res.send({
+         quotes: quotesByPerson
+      });
+   } else {
+      res.send({
+         quotes: quotes
+      });
+   }
+});
+
+app.post("/api/quotes", (req, res) => {
+   const newQuote = {
+      quote: req.query.quote,
+      person: req.query.person
+   };
+   if (newQuote.quote && newQuote.person) {
+      quotes.push(newQuote);
+      res.send({ quote: newQuote });
+   } else {
+      res.status(400).send();
+   }
+});
+
+app.listen(PORT, () => {
+   console.log(`It's alive on http://localhost:${PORT}`);
+});
